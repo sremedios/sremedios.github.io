f264a912ced113289455b2bc5bba7384918b30de
sremedios <sam.remedios@gmail.com>
2020-06-03 10:54:30 -0400

    using web-sys to draw to canvas

--- qa/src/lib.rs
+++ qa/src/lib.rs
@@ -7,13 +7,17 @@ mod utils;
 use nifti::{NiftiObject, InMemNiftiObject, NiftiVolume};
 use nifti::volume::ndarray::IntoNdArray;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::Clamped;
+use web_sys::{CanvasRenderingContext2d, ImageData};
 use ndarray::*;
 use ndarray_stats::*;
 use std::fmt;
+use std::ops::Add;
 use std::io::Cursor;
 use std::fs::File;
 use std::env;
 use std::iter;
+use std::f64;
 use std::path::{Path, PathBuf};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -28,6 +32,96 @@ extern "C" {
     #[wasm_bindgen(js_namespace = console)]
     fn log(s: &str);
 }
+
+// Testing canvas drawing with Julia set
+#[wasm_bindgen]
+pub fn draw(
+    ctx: &CanvasRenderingContext2d,
+    width: u32,
+    height: u32,
+    real: f64,
+    imaginary: f64,
+) -> Result<(), JsValue> {
+    let c = Complex { real, imaginary };
+    let mut data = get_julia_set(width, height, c);
+    let data = ImageData::new_with_u8_clamped_array_and_sh(Clamped(&mut data), width, height)?;
+    ctx.put_image_data(&data, 0.0, 0.0)
+}
+
+fn get_julia_set(width: u32, height: u32, c: Complex) -> Vec<u8> {
+    let mut data = Vec::new();
+
+    let param_i = 1.5;
+    let param_r = 1.5;
+    let scale = 0.005;
+
+    for x in 0..width {
+        for y in 0..height {
+            let z = Complex {
+                real: y as f64 * scale - param_r,
+                imaginary: x as f64 * scale - param_i,
+            };
+            let iter_index = get_iter_index(z, c);
+            data.push((iter_index / 4) as u8);
+            data.push((iter_index / 2) as u8);
+            data.push(iter_index as u8);
+            data.push(255);
+        }
+    }
+
+    data
+}
+
+fn get_iter_index(z: Complex, c: Complex) -> u32 {
+    let mut iter_index: u32 = 0;
+    let mut z = z;
+    while iter_index < 900 {
+        if z.norm() > 2.0 {
+            break;
+        }
+        z = z.square() + c;
+        iter_index += 1;
+    }
+    iter_index
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Complex {
+    real: f64,
+    imaginary: f64,
+}
+
+impl Complex {
+    fn square(self) -> Complex {
+        let real = (self.real * self.real) - (self.imaginary * self.imaginary);
+        let imaginary = 2.0 * self.real * self.imaginary;
+        Complex { real, imaginary }
+    }
+
+    fn norm(&self) -> f64 {
+        (self.real * self.real) + (self.imaginary * self.imaginary)
+    }
+}
+
+impl Add<Complex> for Complex {
+    type Output = Complex;
+
+    fn add(self, rhs: Complex) -> Complex {
+        Complex {
+            real: self.real + rhs.real,
+            imaginary: self.imaginary + rhs.imaginary,
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
 
 #[wasm_bindgen]
 pub struct Lightbox {
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,11 @@ module.exports = {
     path: path.resolve(__dirname, "."),
     filename: "bootstrap.js",
   },
+  entry: "./bootstrap_drawing.js",
+  output: {
+    path: path.resolve(__dirname, "."),
+    filename: "bootstrap_drawing.js",
+  },
   mode: "development",
   plugins: [
     new CopyWebpackPlugin(['index.html']),
