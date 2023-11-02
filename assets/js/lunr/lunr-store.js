46b057993185acd08599eb7ae505e95f36539963
sremedios <sremedios@users.noreply.github.com>
2023-11-02 07:09:45 +0000

    deploy: 4898beb151c8bb20cca21f5380c99b9e459a75bc

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Placeholder Blog",
+        "excerpt":"… rest of post content …  ","categories": [],
+        "tags": [],
+        "url": "/2023/11/01/placeholder-blog.html",
+        "teaser": null
+      }]
